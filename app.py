12741c0fd390281d8cc52b959ebbad8a3e639f00
TarasWRS <119341926+TarasWRS@users.noreply.github.com>
2024-10-26 16:05:56 +0300

    Add new file app.py

--- app.py
+++ app.py
@@ -1,5 +1,1 @@
-print("I'm learning Git merge conflicts")
-name = input('Please enter your name: ')
-last_name = input('Please enter your last name: ')
-nick = input('Please enter your nickname: ')
-print('Hi, {} {}! a.k.a. {}!'.format(name, last_name, nick))
+
