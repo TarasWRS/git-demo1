070aa7395efac46261bb8a5c706792cddd0105ed
TarasWRS <119341926+TarasWRS@users.noreply.github.com>
2024-10-26 14:38:38 +0300

    Add app.py file

--- app.py
+++ app.py
@@ -3,12 +3,3 @@ name = input('Please enter your name: ')
 last_name = input('Please enter your last name: ')
 nick = input('Please enter your nickname: ')
 print('Hi, {} {}! a.k.a. {}!'.format(name, last_name, nick))
-<<<<<<< HEAD
-name = input('Please enter your name: ')
-last_name = input('Please enter your last name: ')
-print('Hi, {} {}!'.format(name, last_name))
-=======
-nick = input('Please enter your nickname: ')
-print('Hi, {}!'.format(nick))
->>>>>>> room15
-print("I'm learning Git merge conflicts")
